7398e83170327f396542b2e2eaaaab0a1b9c2b38
macmade <macmade@xs-labs.com>
2014-09-25 23:30:15 +0200

    Projects page.

--- en/projects/index.php
+++ en/projects/index.php
@@ -2,150 +2,119 @@
     All OpenSource projects are currently hosted on <a href="http://github.com/macmade">GitHub</a>.<br />
     <a href="http://github.com/macmade">Feel free to fork!</a>
 </div>
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-monitor-wallpaper">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/xeos' ); ?>
-                </h3>
+<div class="marketing">
+    <div class="row">
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>"><img src="/uploads/image/xeos/icon-circle.png" alt="XEOS" width="140" height="140" /></a>
             </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>"><img src="/uploads/image/xeos/icon-small.png" alt="XEOS" width="100" height="100" class="pull-right" /></a>
-                XEOS is an experimental 32/64 bits Operating System for x86 platforms, written from scratch in Assembly and C.<br />
-                It includes a C99 Standard Library, and aims at POSIX/SUS2 compatibility.<br />
-                Its main purpose is educationnal, and to provide people interested in OS development with a clean code base.<br />
-                While available only for x86, it may evolve to support other platforms.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>">Learn more...</a>
-                </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/xeos' ); ?></h3>
+            <p>
+                XEOS is an experimental 32/64 bits Operating System for x86 platforms, written from scratch in Assembly and C.
+                It includes a C99 Standard Library, and aims at POSIX/SUS2 compatibility.
+                Its main purpose is educationnal, and to provide people interested in OS development with a clean code base.
+                While available only for x86, it may evolve to support other platforms.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xsfoundation' ); ?>"><img src="/uploads/image/xsfoundation/icon-circle.png" alt="XSFoundation" width="140" height="140" /></a>
             </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/xsfoundation' ); ?></h3>
+            <p>
+                The XSFoundation C Library provides the base for object-oriented C style coding, reference counting memory management with auto-release capabilities, reflection, runtime environment, polymorphism, exceptions, and basic objects.
+                While available as a standalone, cross-platform library, its main purpose is to be integrated in the XEOS Operating System.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xsfoundation' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/codeine' ); ?>"><img src="/uploads/image/codeine/icon-circle.png" alt="Codeine" width="140" height="140" /></a>
+            </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/codeine' ); ?></h3>
+            <p>
+                Codeine is a new code editor for Mac, allowing editing, building, running and debugging C, C++ and Objective-C code.
+                While not an IDE (yet), Codeine aims to evolve to support complex application projects.
+                Codeine uses the latest technologies in source code compilation to provide users with the best environment to build and run software on the Mac platform.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/codeine' ); ?>">Learn more »</a>
+            </p>
         </div>
     </div>
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-library">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/xsfoundation' ); ?>
-                </h3>
+    <div class="row">
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/cfpp' ); ?>"><img src="/uploads/image/cfpp/icon-circle.png" alt="CoreFoundation++" width="140" height="140" /></a>
             </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xsfoundation' ); ?>"><img src="/uploads/image/xsfoundation/icon-small.png" alt="XSFoundation" width="100" height="100" class="pull-right" /></a>
-                The XSFoundation C Library provides the base for object-oriented C style coding, reference counting memory management with auto-release capabilities, reflection, runtime environment, polymorphism, exceptions, and basic objects.<br />
-                While available as a standalone, cross-platform library, its main purpose is to be integrated in the XEOS Operating System.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xsfoundation' ); ?>">Learn more...</a>
-                </div>
-            </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/cfpp' ); ?></h3>
+            <p>
+                CoreFoundation++ (CFPP) is a C++ wrapper for Apple's CoreFoundation API.
+                CoreFoundation++ uses the good parts of the C++ language to wrap the most used CoreFoundation classes, and to makes them easy to use, for instance by using operator overloading.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/cfpp' ); ?>">Learn more »</a>
+            </p>
         </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-design-blueprint">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/codeine' ); ?>
-                </h3>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/clang-kit' ); ?>"><img src="/uploads/image/clang-kit/icon-circle.png" alt="ClangKit" width="140" height="140" /></a>
             </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/codeine' ); ?>"><img src="/uploads/image/codeine/icon-small.png" alt="Codeine" width="100" height="100" class="pull-right" /></a>
-                Codeine is a new code editor for Mac, allowing editing, building, running and debugging C, C++ and Objective-C code.<br />
-                While not an IDE (yet), Codeine aims to evolve to support complex application projects.<br />
-                Codeine uses the latest technologies in source code compilation to provide users with the best environment to build and run software on the Mac platform.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/codeine' ); ?>">Learn more...</a>
-                </div>
-            </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/clang-kit' ); ?></h3>
+            <p>
+                ClangKit provides an Objective-C frontend to LibClang, providing source code tokenization, diagnostics and fix-its.
+                ClangKit is intended to be used as a private framework, in an application's bundle.
+                Possible applications includes source code syntax highlighting, source code tokenization, source code diagnostics and source code static analysis.
+                ClangKit is currently used by the Codeine project.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/clang-kit' ); ?>">Learn more »</a>
+            </p>
         </div>
-    </div>
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-filetype-blueprint-under-construction">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/cfpp' ); ?>
-                </h3>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/filesystem' ); ?>"><img src="/uploads/image/filesystem/icon-circle.png" alt="FileSystem" width="140" height="140" /></a>
             </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/cfpp' ); ?>"><img src="/uploads/image/cfpp/icon-small.png" alt="CoreFoundation++" width="100" height="100" class="pull-right" /></a>
-                CoreFoundation++ (CFPP) is a C++ wrapper for Apple's CoreFoundation API.<br />
-                CoreFoundation++ uses the good parts of the C++ language to wrap the most used CoreFoundation classes, and to makes them easy to use, for instance by using operator overloading.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/cfpp' ); ?>">Learn more...</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-filetype-blueprint-under-construction">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/clang-kit' ); ?>
-                </h3>
-            </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/clang-kit' ); ?>"><img src="/uploads/image/clang-kit/icon-small.png" alt="ClangKit" width="100" height="100" class="pull-right" /></a>
-                ClangKit provides an Objective-C frontend to LibClang, providing source code tokenization, diagnostics and fix-its.<br />
-                ClangKit is intended to be used as a private framework, in an application's bundle.<br />
-                Possible applications includes source code syntax highlighting, source code tokenization, source code diagnostics and source code static analysis.<br />
-                ClangKit is currently used by the Codeine project.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/clang-kit' ); ?>">Learn more...</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-iphone-locked">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/filesystem' ); ?>
-                </h3>
-            </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/filesystem' ); ?>"><img src="/uploads/image/filesystem/icon-small.png" alt="FileSystem" width="100" height="100" class="pull-right" /></a>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/filesystem' ); ?></h3>
+            <p>
                 FileSystem is an application that allows you to browse the content of your iPhone disk, displaying file and folders, files contents, and detailed informations about file and folder permissions.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/filesystem' ); ?>">Learn more...</a>
-                </div>
-            </div>
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/filesystem' ); ?>">Learn more »</a>
+            </p>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-pen">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/xeos-software-license' ); ?>
-                </h3>
+    <div class="row">
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos-software-license' ); ?>"><img src="/uploads/image/xeos-software-license/icon-circle.png" alt="XEOS Software License" width="140" height="140" /></a>
             </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos-software-license' ); ?>"><img src="/uploads/image/xeos-software-license/icon-small.png" alt="XEOS Software License" width="100" height="100" class="pull-right" /></a>
-                The XEOS Software License is an attempt to create a new kind of permissive OpenSource license.<br />
-                Once finalized, it should be the OpenSource license used for all XS-Labs projects.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos-software-license' ); ?>">Learn more...</a>
-                </div>
-            </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/xeos-software-license' ); ?></h3>
+            <p>
+                The XEOS Software License is an attempt to create a new kind of permissive OpenSource license.
+                Once finalized, it should be the OpenSource license used for all XS-Labs projects.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/xeos-software-license' ); ?>">Learn more »</a>
+            </p>
         </div>
-    </div>
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="project-icons-project">
-                    <?php print XS_Menu::getInstance()->getPageLink( '/projects/coding-style-guide' ); ?>
-                </h3>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/coding-style-guide' ); ?>"><img src="/uploads/image/coding-style-guide/icon-circle.png" alt="Coding Style Guide" width="140" height="140" /></a>
             </div>
-            <div class="panel-body">
-                <a href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/coding-style-guide' ); ?>"><img src="/uploads/image/coding-style-guide/icon-small.png" alt="Coding Style Guide" width="100" height="100" class="pull-right" /></a>
-                XS-Labs coding style guide for C, C++, Objective-C and x86 assembly.<br /><br />
-                <div class="text-left">
-                    <a class="btn btn-primary btn-sm" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/coding-style-guide' ); ?>">Learn more...</a>
-                </div>
-            </div>
+            <h3><?php print XS_Menu::getInstance()->getPageLink( '/projects/coding-style-guide' ); ?></h3>
+            <p>
+                XS-Labs coding style guide for C, C++, Objective-C and x86 assembly.
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print XS_Menu::getInstance()->getPageUrl( '/projects/coding-style-guide' ); ?>">Learn more »</a>
+            </p>
         </div>
+        <div class="col-sm-4"></div>
     </div>
 </div>
