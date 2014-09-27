0f13be3e415af0a5e258b9d54171227a8e0439b0
macmade <macmade@xs-labs.com>
2014-09-27 09:16:01 +0200

    Credits page...

--- en/legal/credits/index.php
+++ en/legal/credits/index.php
@@ -13,15 +13,21 @@
 <h3>Graphic content</h3>
 <ul>
     <li>
-        &laquo;Project Icons&raquo;, by <a href="http://bogo-d.deviantart.com/">Mihaiciuc Bogdan</a>, licensed under the terms of the <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0</a> license.
+        &laquo;<a href="http://www.glyphish.com">Glyphish Icons</a>&raquo; - commercial.
+    </li>
+    <li>
+        &laquo;Project Icons&raquo;, by <a href="http://bogo-d.deviantart.com/">Mihaiciuc Bogdan</a> - licensed under the terms of the <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0</a> license.
     </li>
 </ul>
-<h3>JavaScript</h3>
+<h3>JavaScript &amp; CSS</h3>
 <ul>
     <li>
-        <a href="http://jquery.com">jQuery</a>, by the jQuery Foundation, licensed under the terms of the <a href="http://opensource.org/licenses/MIT">MIT</a> license.
+        <a href="http://jquery.com">jQuery</a>, by the jQuery Foundation - licensed under the terms of the <a href="http://opensource.org/licenses/MIT">MIT</a> license.
     </li>
     <li>
-        <a href="http://www.shadowbox-js.com/">ShadowBox</a>, by Michael J. I. Jackson, licensed under the terms of the <a href="http://www.shadowbox-js.com/LICENSE">ShadowBox.js</a> license.
+        <a href="http://getbootstrap.com/">Bootstrap</a> - licensed under the terms of the <a href="http://opensource.org/licenses/MIT">MIT</a> license.
+    </li>
+    <li>
+        <a href="http://osvaldas.info/image-lightbox-responsive-touch-friendly">Image Lightbox</a>, by Osvaldas Valutis - licensed under the terms of the <a href="http://opensource.org/licenses/MIT">MIT</a> license.
     </li>
 </ul>
